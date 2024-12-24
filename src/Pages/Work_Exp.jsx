116df95bf1f926c3dcb9dadd3dac162e7d4c6554
personalGitprofile <shahidulislam.personal1@gmail.com>
2024-12-24 12:22:30 +0600

    m portfolio

---
+++ src/Pages/Work_Exp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Work_Exp = () => {
+    return (
+        <div>
+            <h1>Experiance Portfolio</h1>
+        </div>
+    );
+};
+
+export default Work_Exp;
---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root/Root.jsx';
+import Home from './Pages/Home.jsx';
+import Work_Exp from './Pages/Work_Exp.jsx';
+import About from './Pages/About.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        path:"/portfolion",
+        element:<Work_Exp></Work_Exp>
+
+      },
+      {
+        path:"/about",
+        element:<About></About>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  <RouterProvider router={router} />
+  </StrictMode>,
+)
